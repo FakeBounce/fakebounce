e2d7ccf9482239a242ee7df876d512005be7f691
Fakebounce <romanellibenjamin66@gmail.com>
2024-01-16 10:32:45 +0100

    Added firebase deployment

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,25 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { initializeApp } from 'firebase/app';
 import reportWebVitals from './reportWebVitals';
 import { store } from './app/store';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP__APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+// const analytics = getAnalytics(firebaseApp); // Getting analytics
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
